ab2556a8837005688e3aa7d2371a6a64dfeafa1d
Alexandr Duboshin <superfozi@mail.ru>
2024-09-23 20:29:52 +0300

    losson1

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "titleProject";
+let screens = "screensProject";
+let screenPrice = "screen Price";
+let rollback = "rollbackProject";
+let fyllPrice = "full Prace";
+let adaptive = "adaptiveProject";
+
+alert("Hellow");
+console.log("Всем привет!");
