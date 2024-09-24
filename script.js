f2046ba7d7048868b0f43aec8529ae4e6daa74f4
Bad-fozi <72658903+Bad-fozi@users.noreply.github.com>
2024-09-24 20:15:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let title = "titleProject";
 let screens = "screensProject";
 let screenPrice = "screen Price";
 let rollback = "rollbackProject";
-let fyllPrice = "full Prace";
+let fullPrice = "full Prace";
 let adaptive = "adaptiveProject";
 
 alert("Hellow");
